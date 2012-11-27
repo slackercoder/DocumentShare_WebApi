da6e2dd0114c1ec49b8297466efdf87783c6ae79
Mario Folino <mario.folino@gmail.com>
2012-11-26 21:26:51 -0500

    Moved the fake service out to a new database action

--- DocumentShare/Controllers/DocumentController.cs
+++ DocumentShare/Controllers/DocumentController.cs
@@ -10,22 +10,20 @@ namespace DocumentShare.Controllers
 {
     public class DocumentController : ApiController
     {
-        private List<Document> documents = new List<Document>()
-        {
-            new Document() {DocumentId = 1, DocumentName="Doc1", DocumentDescription="Doc1Desc", DocumentLocation="Doc1Loc"},
-            new Document() {DocumentId = 2, DocumentName="Doc2", DocumentDescription="Doc2Desc", DocumentLocation="Doc2Loc"},
-            new Document() {DocumentId = 3, DocumentName="Doc3", DocumentDescription="Doc3Desc", DocumentLocation="Doc3Loc"},
-            new Document() {DocumentId = 4, DocumentName="Doc4", DocumentDescription="Doc4Desc", DocumentLocation="Doc4Loc"}
-        };
-
         public IEnumerable<Document> GetAllDocuments()
         {
-            return documents;
+            using (DocumentShareEntities data = new DocumentShareEntities())
+            {
+                return data.Documents.ToList();
+            }
         }
 
         public Document GetDocumentbyId(int id)
         {
-            return documents.Where(o => o.DocumentId == id).First();
+            using (DocumentShareEntities data = new DocumentShareEntities())
+            {
+                return data.Documents.Where(o => o.DocumentId == id).FirstOrDefault();
+            }
         }
     }
 }
---
+++ DocumentShare/Models/DocumentShareModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Mario\Documents\Visual Studio 2012\Projects\DocumentShare_WebApi\DocumentShare\Models\DocumentShareModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
